6cf5d5d7c650c4b6bbd2502121c9d9d11babf991
Hickey, Lionel <Lionel.Hickey@sew.com.au>
2018-06-07 16:59:34 +1000

    added cors

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var express = require("express");
 var JsonDB = require("node-json-db");
 var path = require("path");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 var graphql_server_express_1 = require("graphql-server-express");
 var graphql_tools_1 = require("graphql-tools");
 var types_1 = require("./types");
@@ -17,6 +18,7 @@ var schema = graphql_tools_1.makeExecutableSchema({
     resolvers: resolvers
 });
 var homePath = '/graphiql';
+app.use(cors());
 app.use('/graphql', bodyParser.json(), graphql_server_express_1.graphqlExpress({ schema: schema }));
 app.use(homePath, graphql_server_express_1.graphiqlExpress({ endpointURL: '/graphql' }));
 app.use(express.static(__dirname + '/www'));
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as JsonDB from 'node-json-db';
 import * as path from 'path';
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 
@@ -21,6 +22,8 @@ const schema = makeExecutableSchema({
 });
 
 const homePath = '/graphiql'    
+
+app.use(cors());
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
 app.use(homePath, graphiqlExpress({ endpointURL: '/graphql' }))
